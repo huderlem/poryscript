f704ef7a2bef4bc2094be543c04bb41bc308402a
hjk321 <anderdude@outlook.com>
2021-02-22 15:16:01 -0600

    agbcc style install.sh
    This adds a shell script to easily install a built-from-source poryscript executable (and the font widths file) into a project. This is similar to how agbcc is installed.

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+if [ "$1" != "" ]; then
+    mkdir -p $1/tools/poryscript
+    if test -f "poryscript"; then
+        cp poryscript $1/tools/poryscript
+    elif test -f "poryscript.exe"; then
+        cp poryscript.exe $1/tools/poryscript
+    else
+        echo "Could not find executable to install. Try building first!"
+        exit 1
+    fi
+    cp font_widths.json $1/tools/poryscript
+else
+    echo "Usage: install.sh PATH"
+fi
